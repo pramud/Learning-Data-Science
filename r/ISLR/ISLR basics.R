586cf7943833bf4a87db165a90abc56f9d332067
pramud <naga92@gmail.com>
2016-11-07 23:04:58 +0530

    ISLR First excercises

--- r/ISLR/ISLR basics.R
+++ r/ISLR/ISLR basics.R
@@ -1,3 +1,14 @@
 install.packages("ISLR")
 library(ISLR)
 fix(Auto)
+plot(Auto$cylinders,Auto$mpg)
+attach(Auto)
+plot(cylinders,mpg)
+#If the variable plotted on the x-axis is categorial, then boxplots will
+#automatically be produced by the plot() function. As usual, a number of options can be speci???ed in order to customize the plots
+cylinders =as.factor(cylinders )
+plot(cylinders , mpg , col="red")
+plot(cylinders , mpg , col="red", varwidth =T)
+#The hist() function can be used to plot a histogram
+hist(mpg)
+summary(mpg)
