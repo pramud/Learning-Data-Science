f008340326a97424fc7739b215fec8650cac9675
pramud <naga92@gmail.com>
2016-11-08 23:41:10 +0530

    some more commands on college data

--- r/ISLR/CollegeDataSet.R
+++ r/ISLR/CollegeDataSet.R
@@ -1,3 +1,11 @@
 library(ISLR)
 summary(College)
 names(College)
+fix(College)
+attach(College)
+rownames(College)
+College [,1] 
+pairs(College)
+
+Elite=rep("No",nrow(College )) 
+Elite[College$Top10perc >50]=" Yes" 
