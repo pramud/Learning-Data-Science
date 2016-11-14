30834bb993edf093326ada740eaa8e66cabf4704
Pramud Bommakanti <pramud@outlook.com>
2016-11-14 13:56:59 +0530

    Create learning.py
    All learnings at a single place

---
+++ learning.py
@@ -0,0 +1,6 @@
+'''The number of parameters (and consequently, the amount of training
+data needed to adequately estimate them) would rapidly grow if we add more
+features or higher order terms. This phenomenon, present in every machine learning
+method, is called the idem curse of dimensionality: when the number of parameters
+of a model grows, the data needed to learn them grows exponentially
+'''
