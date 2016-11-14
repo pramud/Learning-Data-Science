6ae442819dcbc2b164f55fa9f77d0a9b5cc15439
Pramud Bommakanti <pramud@outlook.com>
2016-11-14 11:35:33 +0530

    Update exploringSkLearn.py
    Added more comments

--- python/basics/exploringSkLearn.py
+++ python/basics/exploringSkLearn.py
@@ -8,3 +8,17 @@ X_iris, y_iris = iris.data, iris.target
 
 print X_iris.shape, y_iris.shape
 print X_iris[0], y_iris[0]
+
+#iris dataset is an object (similar to a dictionary) that has two main components
+
+#A data array, where, for each instance, we have the real values for sepal
+#length, sepal width, petal length, and petal width, in that order (note that for
+#efficiency reasons, scikit-learn methods work on NumPy ndarrays instead of
+#the more descriptive but much less efficient Python dictionaries or lists). The
+#shape of this array is (150, 4), meaning that we have 150 rows (one for each
+#instance) and four columns (one for each feature).
+
+#A target array, with values in the range of 0 to 2, corresponding to each
+#instance of Iris species (0: setosa, 1: versicolor, and 2: virginica), as you can
+#verify by printing the iris.target.target_names value.
+
