ab35a779c52866ca1188876fc83d73377c808860
Pramud Bommakanti <pramud@outlook.com>
2016-11-15 09:10:14 +0530

    Update SVM_image_recognition.py

--- python/SVM_image_recognition.py
+++ python/SVM_image_recognition.py
@@ -41,3 +41,39 @@ def print_faces(images, target, top_n):
     p.text(0, 60, str(i))
     
 print_faces(faces.images, faces.target, 20)
+
+#Training a Support Vector Machine
+
+from sklearn.svm import SVC
+
+'''The SVC implementation has different important parameters; probably the most
+relevant is kernel, which defines the kernel function to be used in our classifier
+(think of the kernel functions as different similarity measures between instances).
+By default, the SVC class uses the rbf kernel, which allows us to model nonlinear
+problems. To start, we will use the simplest kernel, the linear one.'''
+
+svc_1 = SVC(kernel='linear')
+
+'''Before continuing, we will split our dataset into training and testing datasets.'''
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(faces.data, faces.target, test_size=0.25, random_state=0)
+
+'''And we will define a function to evaluate K-fold cross-validation.'''
+
+from sklearn.cross_validation import cross_val_score, KFold
+from scipy.stats import sem
+
+def evaluate_cross_validation(clf, X, y, K):
+  # create a k-fold croos validation iterator
+  cv = KFold(len(y), K, shuffle=True, random_state=0)
+  # by default the score used is the one returned by score method of the estimator (accuracy)
+  scores = cross_val_score(clf, X, y, cv=cv)
+  print scores
+  print ("Mean score: {0:.3f} (+/-{1:.3f})").format( np.mean(scores), sem(scores))
+
+evaluate_cross_validation(svc_1, X_train, y_train, 5)
+
+'''Cross-validation with five folds, obtains pretty good results (accuracy of 0.933). In a
+few steps we obtained a face classifier.'''
+
