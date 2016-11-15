3bfafc99c1245ae8b50c75854de1db0598223f6d
Pramud Bommakanti <pramud@outlook.com>
2016-11-15 13:04:49 +0530

    Update links.py

--- links.py
+++ links.py
@@ -1,2 +1,3 @@
 '''http://web.mit.edu/~csvoss/Public/usabo/stats_handout.pdf'''
 '''https://journeyofanalytics.wordpress.com/2016/06/23/cheatsheet-selecting-graphs-for-statistical-analysis/'''
+'''http://www.skillsyouneed.com/num/graphs-charts.html'''
