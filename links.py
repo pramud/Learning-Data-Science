dc961484de0602ba747a1208807d2789ea0ed0fe
Pramud Bommakanti <pramud@outlook.com>
2016-11-16 08:46:42 +0530

    Update links.py

--- links.py
+++ links.py
@@ -1,3 +1,4 @@
 '''http://web.mit.edu/~csvoss/Public/usabo/stats_handout.pdf'''
 '''https://journeyofanalytics.wordpress.com/2016/06/23/cheatsheet-selecting-graphs-for-statistical-analysis/'''
 '''http://www.skillsyouneed.com/num/graphs-charts.html'''
+'''https://github.com/justmarkham/DAT4/blob/master/notebooks/08_linear_regression.ipynb'''
